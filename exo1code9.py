1264e9e3e726db2be57f842712117786e603ce91
ROSULME <125694750+ROSULME@users.noreply.github.com>
2023-11-30 14:53:52 -0500

    Add files via upload

---
+++ exo1code9.py
@@ -0,0 +1,5 @@
+karakte_majiskil=input("antre yon chenn karakte majiskil :")
+print(karakte_majiskil)
+karakte_miniskil=karakte_majiskil.lower()
+
+print(F"an miniskil chenn karakte ou te antre an se:{karakte_miniskil}")
