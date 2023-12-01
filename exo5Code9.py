f4ad2478494a4352e8e4bc9e98a7c775bdd752fe
ROSULME <125694750+ROSULME@users.noreply.github.com>
2023-12-01 04:59:16 -0500

    Add files via upload

---
+++ exo5Code9.py
@@ -0,0 +1,6 @@
+antreUser=input(" antre yon chenn karakte :")
+chanjmanLet = "@"
+
+# Remplacer toutes les occurrences de "a" (minuscule) par le symbole
+nouvoChenn= antreUser.replace("a", chanjmanLet)
+print(nouvoChenn)
